f144181ab3eb802189971288a15c0cb0727a25a0
soumenpasari-grokr <soumen.pasari@datagrokr.com>
2019-12-27 14:01:53 +0530

    feat : string exercise tests approved

--- src/string_exercise.py
+++ src/string_exercise.py
@@ -7,25 +7,38 @@ class StringExercise:
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        return input_str[::-1]
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
+        vowels_list = ['a','e','i','o','u']
+        if character.lower() in vowels_list:
+            return True
+        else:
+            return False
 
     def find_longest_word(self, sentence):
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        sentence = sentence.split(' ')
+        caught_word = {'max':0,'index':None}
+        for index_point,word in enumerate(sentence):
+            if len(word) > caught_word['max']:
+                caught_word['max'] = len(word)
+                caught_word['index'] = index_point
+        return sentence[caught_word['index']]
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        result_list = []
+        for word in text.split(' '):
+            result_list.append(len(word))
+        return result_list
